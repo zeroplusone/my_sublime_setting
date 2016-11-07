e79ea0ae1887fd3c4c911c9713a5e8450ccbf2f0
zeroplusone <a711186@gmail.com>
2016-11-07 16:56:01 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# my_sublime_setting
+
+##Environment
+- **OS** : macOS Sierra 10.12.1
+- **software** : sublime Text 3
+
+##file description
+- **Default (OSX).sublime-keymap** : custom shortcuts setup
+
+- **[Evernote.sublime-settings](https://packagecontrol.io/packages/Evernote)**	: This package can translate Markdown in sublime to Evernote, and the setting file define some font and layout in css.
+
+- **Preferences.sublime-settings** : sublime setting file which define theme and tab setting in custom
+
+- **weekly_report.py** : custom plugin to create my weekly report template which developed by me
---
+++ weekly_report.py
@@ -0,0 +1,61 @@
+import sublime
+import sublime_plugin
+from datetime import datetime, timedelta
+
+"""
+This class is my custom plug-in in sublime Text 3.
+This 'weekly_report' command is used to create template of weekly report.
+"""
+
+
+class WeeklyReportCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        monday = self.find_monday(datetime.today())
+        sunday = monday + timedelta(days=6)
+        title = monday.strftime("%Y/%m/%d") + "~" + sunday.strftime("%Y/%m/%d")
+
+        content = self.edit_content(monday, title)
+        contents = ""
+        for str in content:
+            contents += str
+            # self.view.insert(edit, 0, str)
+        self.view.run_command('insert_snippet', {'contents': contents})
+
+    def find_monday(self, today):
+        if today.weekday() > 2:
+            return today - timedelta(days=7)
+        else:
+            return today - timedelta(days=7) - timedelta(days=today.weekday())
+
+    def edit_content(self, monday, title):
+        content = []
+        content.append("---\n")
+        content.append("title:" + title + "\n")
+        content.append("notebook: weekly report\n")
+        content.append("tags:\n")
+        content.append("---\n\n")
+        content.append("- [ ]\n")
+        content.append("---------\n")
+
+        for i in range(0, 6):
+            day = monday + timedelta(days=i)
+            content.append("- " + day.strftime("%m/%d") + "\n")
+            content.append("    - \n")
+
+        content.append("---------\n")
+        content.append("Subject: [IMSLab][Weekly Report] 林佳瑩 " + title + "\n")
+        content.append("老師：\n這禮拜主要在")
+        content.append("1.\n")
+        content.append("2.\n")
+        content.append("3.\n")
+        content.append("[本週進度]\n")
+        content.append("1.\n")
+        content.append("2.\n")
+        content.append("[下週進度]\n")
+        content.append("1.\n")
+        content.append("2.\n")
+        content.append("[Meeting Note]")
+        content.append("- \n")
+        content.append("小六\n")
+        return content
